a5efeacb46046e380e30a21c2ee4e22da8ad411a
resourceflag2 <79238263+resourceflag2@users.noreply.github.com>
2021-02-23 12:48:59 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,36 @@
+function p1 () {
+document.getElementById('mya1').href='http://jiexi.bbbjw.com/dm/?url=https://douban.donghongzuida.com/20210215/17440_4d6ee640/index.m3u8'
+}
+function p2 () {
+document.getElementById('mya2').href='http://jiexi.bbbjw.com/dm/?url=https://qingxi.xi-zuida.com/20210214/1241_b3b11edb/index.m3u8'
+}
+function p3 () {
+document.getElementById('mya3').href='http://jiexi.bbbjw.com/dm/?url=https://xigua-cdn.haima-zuida.com/20210214/19906_da9f65bd/index.m3u8'
+}
+function p4 () {
+document.getElementById('mya4').href='http://jiexi.bbbjw.com/dm/?url=https://douban.donghongzuida.com/20210214/17395_b04a72b3/index.m3u8'
+}
+function p5 () {
+document.getElementById('mya5').href='http://jiexi.bbbjw.com/dm/?url=https://meinv.jingyu-zuida.com/20210214/14228_c0ae9bf2/index.m3u8'
+}
+function p6 () {
+document.getElementById('mya6').href='http://jiexi.bbbjw.com/dm/?url=https://chaoqing.dong-zuida.com/20210207/1109_d11409d6/index.m3u8'
+}
+function p7 () {
+document.getElementById('mya7').href='http://jiexi.bbbjw.com/dm/?url=https://chaoqing.dong-zuida.com/20210207/1106_c79f9dde/index.m3u8'
+}
+function p8 () {
+document.getElementById('mya8').href='http://jiexi.bbbjw.com/dm/?url=https://chaoqing.dong-zuida.com/20210207/1095_a1f80509/index.m3u8'
+}
+function p9 () {
+document.getElementById('mya9').href='http://jiexi.bbbjw.com/dm/?url=https://xigua-cdn.haima-zuida.com/20210212/19868_66205101/index.m3u8'
+}
+function p10 () {
+document.getElementById('mya10').href='http://jiexi.bbbjw.com/dm/?url=https://chaoqing.dong-zuida.com/20210207/1094_5f7c2452/index.m3u8'
+}
+function p11 () {
+document.getElementById('mya11').href='http://jiexi.bbbjw.com/dm/?url=https://meinv.jingyu-zuida.com/20210212/14220_9ad68853/index.m3u8'
+}
+function p12 () {
+document.getElementById('mya12').href='http://jiexi.bbbjw.com/dm/?url=https://meinv.jingyu-zuida.com/20210212/14216_7d53df26/index.m3u8'
+}
